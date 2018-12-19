ca71013a8677738587236c9d0088067a18f0b8cf
s greenbaum <gbs4ever2@gmail.com>
2018-12-19 04:04:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def roll_call_dwarves(array)# code an argument here
-array.each_with_index do |x| puts  "#{index+1} #{x}"
+array.each_with_index do |x|  "#{index+1} #{x}"
 binding.pry
 end
 end

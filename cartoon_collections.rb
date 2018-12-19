573a1169f2ed8722d7fa8cfe41ac8aa339bd5fe1
s greenbaum <gbs4ever2@gmail.com>
2018-12-19 04:17:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet(elment)# code an argument here
 elment.each.map {|x|   x.capitalize }  #{}"!}
 end
-puts elment
+
 
 def long_planeteer_calls# code an argument here
   # Your code here

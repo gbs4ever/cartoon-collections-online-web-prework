ee8285c2ca783eae1fdd25330b249e2eb2e72813
s greenbaum <gbs4ever2@gmail.com>
2018-12-19 04:16:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ array.each_with_index  {|item,index|   puts "#{index+1} #{item}"}
 end
 
 def summon_captain_planet(elment)# code an argument here
-update =elment.each.map {|x|   x.capitalize , #{}"!"}
+update =elment.each.map {|x|   x.capitalize }  #{}"!}
 end
 puts update
 
